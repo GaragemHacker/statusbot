1cb5ba2430cd67c6c8e702fd355f0b0aa5d412ac
Igor Brandao <igorsca@gmail.com>
2015-02-27 23:19:38 -0300

    Criacao do status.php

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Clever Joke Bot to make the Garage more acessible in the web
  * HackerSpace Door status
  * Yowsup intergation
  * WebSite and Wiki hackerspace status
+
+### status.php
+
+Para passar o status do hackerspace como aberto, foi optado por usar o status.php que grava um arquivo status.txt com o estado do hackerspace. A requisicao e feito por um arduino com ethernet shield.
+
---
+++ status.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>GaragemHacker Status</title>
+
+<style>
+div.lamp{
+	width:150px;
+	height: 235px;
+	background-image:url('lamp.png');
+	/*background-size: 150px 100px;*/
+	background-repeat: no-repeat;
+    	background-attachment: fixed;
+    	background-position: 15px 15px; 
+}
+</style>
+</head>
+
+<body>
+<h3>Garagem Hacker - Curitiba HackerSpace</h3>
+
+<p>
+<?php
+if ($_GET["operacao"] === "abrir") {
+	echo "aberto\n";
+	// gravar "aceso" no status.txt
+	$status = fopen("status.txt", "w") or die("OPS... nao gravou");	
+	$txt = "aberto\n";
+	fwrite($status, $txt);
+	fclose($status);
+	
+} elseif ($_GET["operacao"] === "fechar") {
+	echo "fechado";
+	// gravar "apagado" no status.txt
+	$status = fopen("status.txt", "w") or die("OPS... nao gravou");	
+	$txt = "fechado\n";
+	fwrite($status, $txt);
+	fclose($status);
+	
+}
+
+?>
+<!--</p>
+
+<ul>
+<li><a href="?operacao=abrir">Abrir</a></li>
+<li><a href="?operacao=fechar">Fechar</a></li>
+</ul>
+-->
+
+
+</body>
