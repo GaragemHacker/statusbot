0f80ee85293d9c5c951527520139c2631dc8899d
Igor Brand <igorsca@gmail.com>
2015-03-18 19:36:40 -0300

    fully working, more debugs

--- lamp.js
+++ lamp.js
@@ -1,27 +1,28 @@
 window.onload=function start() {
 
-var xmlhttp, status; //my vars
+var xmlhttp, status, logger; //my vars
 //var xmlhttp;
 xmlhttp = new XMLHttpRequest();
 //xmlhttp.open('GET', 'http://www.garagemhacker.org/status.txt', true); //this request for outsider's require enable CORS
 xmlhttp.open('GET', 'status.txt', true);
 xmlhttp.send();
 
-status = xmlhttp.responseText;//get the response on status var
+//logger = xmlhttp.responseText;//get the response on status var
 console.log(xmlhttp.responseText);
 xmlhttp.onreadystatechange=function(){
 	
 	status = xmlhttp.responseText;//get the response on status var
 	
  	 if (xmlhttp.readyState==4 && xmlhttp.status==200){
-  		if(xmlhttp.responseText.trim() == 'fechado'){
-    		document.getElementById("lamp").innerHTML=xmlhttp.responseText;
-    		console.log(xmlhttp); //Somente para Debug 
+  		if(status.trim() == 'fechado'){
+    		document.getElementById("lamp").innerHTML=status;
+    		//console.log(xmlhttp); //Somente para Debug 
     		}
-    	else if (xmlhttp.responseText.trim() == 'aberto') {
+    	else if (status.trim() == 'aberto') {
    			//document.getElementById("real").innerText=xmlhttp.responseText;//Use para requisicoes sincronas com xmlhttp false
-    		document.getElementById("lamp").innerHTML=xmlhttp.responseText;
+    		document.getElementById("lamp").innerHTML=status;
     		}
+    	//console.log(xmlhttp);//uncoment for a fully debug for if end esle if codition	
     	}
   	}//close the xmlhttp.onreadystatechange
 }//close start
