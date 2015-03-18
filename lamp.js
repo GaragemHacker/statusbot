c7a6fc26cb2b41db383e5a52011d3bfd7afd0833
Igor Brand <igorsca@gmail.com>
2015-03-18 19:24:44 -0300

    lamp.js fully working

--- lamp.js
+++ lamp.js
@@ -1,24 +1,27 @@
 window.onload=function start() {
 
-//var xmlhttp, text; //algumas variaveis de debug
-var xmlhttp;
+var xmlhttp, status; //my vars
+//var xmlhttp;
 xmlhttp = new XMLHttpRequest();
-xmlhttp.open('GET', 'http://www.garagemhacker.org/status.txt', true);
+//xmlhttp.open('GET', 'http://www.garagemhacker.org/status.txt', true); //this request for outsider's require enable CORS
+xmlhttp.open('GET', 'status.txt', true);
 xmlhttp.send();
 
-//text = xmlhttp.responseText; //get the response on text var
-
+status = xmlhttp.responseText;//get the response on status var
+console.log(xmlhttp.responseText);
 xmlhttp.onreadystatechange=function(){
-  if (xmlhttp.readyState==4 && xmlhttp.status==200){
-  	if(xmlhttp.responseText.trim() == 'fechado'){
-    	document.getElementById("lamp").innerHTML=xmlhttp.responseText;
-    	//console.log(xmlhttp); //Somente para Debug
-    	
+	
+	status = xmlhttp.responseText;//get the response on status var
+	
+ 	 if (xmlhttp.readyState==4 && xmlhttp.status==200){
+  		if(xmlhttp.responseText.trim() == 'fechado'){
+    		document.getElementById("lamp").innerHTML=xmlhttp.responseText;
+    		console.log(xmlhttp); //Somente para Debug 
+    		}
+    	else if (xmlhttp.responseText.trim() == 'aberto') {
+   			//document.getElementById("real").innerText=xmlhttp.responseText;//Use para requisicoes sincronas com xmlhttp false
+    		document.getElementById("lamp").innerHTML=xmlhttp.responseText;
+    		}
     	}
-    else if (xmlhttp.responseText.trim() == 'aberto') {
-   		//document.getElementById("real").innerText=xmlhttp.responseText;//Use para requisicoes sincronas com xmlhttp false
-    	document.getElementById("lamp").innerHTML=xmlhttp.responseText;
-    	}
-    }
-  }
-}
+  	}//close the xmlhttp.onreadystatechange
+}//close start
